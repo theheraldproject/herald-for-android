1e639aa72f9ed9130d9d483b02eaa92e5841182a
C19X <support@c19x.org>
2020-09-16 15:07:27 +0100

    Added mock sonar payload supplier
    - Deleted in previous repo by mistake

---
+++ app/src/main/java/org/c19x/sensor/payload/sonar/SonarPayloadDataSupplier.java
@@ -0,0 +1,33 @@
+package org.c19x.sensor.payload.sonar;
+
+import org.c19x.sensor.datatype.Data;
+import org.c19x.sensor.datatype.PayloadData;
+import org.c19x.sensor.datatype.PayloadTimestamp;
+import org.c19x.sensor.payload.DefaultPayloadDataSupplier;
+
+import java.nio.ByteBuffer;
+
+/// Mock SONAR payload supplier for simulating payload transfer of 129 byte Sonar payload data.
+public class SonarPayloadDataSupplier extends DefaultPayloadDataSupplier {
+    private static final int length = 129;
+    private final int identifier;
+
+    public SonarPayloadDataSupplier(int identifier) {
+        this.identifier = identifier;
+    }
+
+    private Data networkByteOrderData(int identifier) {
+        final ByteBuffer byteBuffer = ByteBuffer.allocate(4);
+        byteBuffer.putInt(0, identifier);
+        return new Data(byteBuffer.array());
+    }
+
+    @Override
+    public PayloadData payload(PayloadTimestamp timestamp) {
+        final ByteBuffer byteBuffer = ByteBuffer.allocate(length);
+        // First 3 bytes are reserved in SONAR
+        byteBuffer.position(3);
+        byteBuffer.put(networkByteOrderData(identifier).value);
+        return new PayloadData(byteBuffer.array());
+    }
+}
