022af0d0711d20c3f7a13310d78ea481889b1b7d
Marcus Pridham <mpridham@vmware.com>
2021-01-28 15:12:23 -0500

    Add ability to format payload data for efficacy logging
    Fixes #152
    
    Signed-off-by: Marcus Pridham <mpridham@vmware.com>

---
+++ herald/src/main/java/com/vmware/herald/sensor/data/ConcretePayloadDataFormatter.java
@@ -0,0 +1,14 @@
+//  Copyright 2021 VMware, Inc.
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+package com.vmware.herald.sensor.data;
+
+import com.vmware.herald.sensor.datatype.PayloadData;
+
+public class ConcretePayloadDataFormatter implements PayloadDataFormatter {
+    @Override
+    public String shortFormat(PayloadData payloadData) {
+        return payloadData.shortName();
+    }
+}
