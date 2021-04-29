db58bdedf1a7f67d168aac59f622f57d4eb9df29
C19X <support@c19x.org>
2021-04-29 11:14:56 +0100

    Added missing copyright header to new file - Validated
    Signed-off-by: c19x <support@c19x.org>

--- herald/src/main/java/com/vmware/herald/sensor/analysis/algorithms/distance/RssiHistogram.java
+++ herald/src/main/java/com/vmware/herald/sensor/analysis/algorithms/distance/RssiHistogram.java
@@ -1,3 +1,7 @@
+//  Copyright 2021 Herald Project Contributors
+//  SPDX-License-Identifier: Apache-2.0
+//
+
 package com.vmware.herald.sensor.analysis.algorithms.distance;
 
 import com.vmware.herald.sensor.DefaultSensorDelegate;
